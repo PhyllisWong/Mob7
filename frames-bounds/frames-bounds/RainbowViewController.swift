1fe3721ca5cb9106d4a269a0509fd70e99e5f968
Phyllis <wong.phyllism@gmail.com>
2018-05-02 12:44:07 -0700

    Add colors enum for creating the rainbow

--- frames-bounds/frames-bounds/RainbowViewController.swift
+++ frames-bounds/frames-bounds/RainbowViewController.swift
@@ -9,27 +9,92 @@
 import UIKit
 
 class RainbowViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        self.view.addSubview(containerVC)
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    lazy var containerVC: UIView = {
+        let width = self.view.frame.width
+        let height = self.view.frame.height
+        
+        var xPos = CGFloat(0)
+        var yPos = CGFloat(0)
+        
+        let rect = CGRect(x: xPos, y: yPos, width: width, height: height)
+        let vc = UIView(frame: rect)
+        vc.backgroundColor = .blue
+        
+        return vc
+    }()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Create a rainbow view with 12 colors
+    // width same as the screen width with a 20px margin
+    // height of the 12 color stack screen height with 20 px margin
+    
+    
 }
+
+enum Colors: UIColor {
+    
+    // Colors to present the rainbow stack
+    case redOrange
+    case orange
+    case yellow
+    case lightGreen
+    case green
+    case greenBlue
+    case lightBlue
+    case blue
+    case violet
+    case pink
+    case redPink
+    case red
+    
+    func toUIColor() -> UIColor {
+        switch self {
+            case .redOrange:
+                return UIColor(r: 255, g: 94, b: 0)
+            case .orange:
+                return UIColor(r: 255, g: 145, b: 0)
+            case .yellow:
+                return UIColor(r: 255, g: 255, b: 0)
+            case .lightGreen:
+                return UIColor(r: 128, g: 255, b: 0)
+            case .green:
+                return UIColor(r: 0, g: 255, b: 0)
+            case .greenBlue:
+                return UIColor(r: 0, g: 255, b: 128)
+            case .lightBlue:
+                return UIColor(r: 0, g: 255, b: 255)
+            case .blue:
+                return UIColor(r: 0, g: 0, b: 255)
+            case .violet:
+                return UIColor(r: 128, g: 0, b: 255)
+            case .pink:
+                return UIColor(r: 255, g: 0, b: 255)
+            case .redPink:
+                return UIColor(r: 255, g: 0, b: 128)
+            case .red:
+                return UIColor(r: 255, g: 0, b: 0)
+        }
+    }
+}
+
+
+extension UIColor {
+    
+    // Extension on colors to make RGB easier to read
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
+
+
+
+
+
+
